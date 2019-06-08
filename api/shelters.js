480c93ee4c4a624684633bfe7e55145d8ff730b4
Sanderhelleso <sanderhelleso@hotmail.com>
2019-06-08 16:23:56 +0200

    Adjusted style and disabled api

--- api/shelters.js
+++ api/shelters.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require('fs');
 const express = require('express');
 const router = express.Router();
 
@@ -9,72 +9,71 @@ const base = `/api/v${version}/shelters`;
 // controller
 const sc = require('../controllers/shelterController');
 
-module.exports = app => {
+module.exports = (app) => {
+	// store data from dataset
+	//sc.seedShelters();
 
-    // store data from dataset
-    sc.seedShelters();
+	// send dataset to client
+	router.get('/dataset', (req, res) => {
+		fs.readFile('./dataset/shelters.json', 'utf8', (err, data) => {
+			if (err) throw err;
+			res.send(data);
+		});
+	});
 
-    // send dataset to client
-    router.get("/dataset", (req, res) => {
-        fs.readFile("./dataset/shelters.json", "utf8", (err, data) => {
-            if (err) throw err;
-            res.send(data);
-        });
-    });
+	/*// get all shelters
+	router.get('/all', async (req, res) => {
+		const result = await sc.getAllShelters(req, res);
+		res.send(result);
+	});
 
-    // get all shelters
-    router.get('/all', async (req, res) => {
-        const result = await sc.getAllShelters(req, res);
-        res.send(result);
-    });
+	// get single shelter by id
+	router.get('/:id', async (req, res) => {
+		const result = await sc.getShelterByID(req, res);
+		res.send(result);
+	});
 
-    // get single shelter by id
-    router.get('/:id', async (req, res) => {
-        const result = await sc.getShelterByID(req, res);
-        res.send(result);
-    });
+	// get shelters matching keywords
+	router.get('/search/:keywords', async (req, res) => {
+		const result = await sc.getSheltersByKeywords(req, res);
+		res.send(result);
+	});
 
-    // get shelters matching keywords
-    router.get('/search/:keywords', async (req, res) => {
-        const result = await sc.getSheltersByKeywords(req, res);
-        res.send(result);
-    });
+	// get shelters matching municipality'
+	router.get('/municipality/:municipality', async (req, res) => {
+		const result = await sc.getSheltersByMunicipality(req, res);
+		res.send(result);
+	});
 
-     // get shelters matching municipality'
-     router.get('/municipality/:municipality', async (req, res) => {
-        const result = await sc.getSheltersByMunicipality(req, res);
-        res.send(result);
-    });
+	// get shelters matching district
+	router.get('/district/:district', async (req, res) => {
+		const result = await sc.getSheltersByDistrict(req, res);
+		res.send(result);
+	});
 
-    // get shelters matching district
-    router.get('/district/:district', async (req, res) => {
-        const result = await sc.getSheltersByDistrict(req, res);
-        res.send(result);
-    });
+	// get shelters with greater or equal areal
+	router.get('/areal/min/:min', async (req, res) => {
+		const result = await sc.getSheltersByMinAreal(req, res);
+		res.send(result);
+	});
 
-    // get shelters with greater or equal areal
-    router.get('/areal/min/:min', async (req, res) => {
-        const result = await sc.getSheltersByMinAreal(req, res);
-        res.send(result);
-    });
+	// get shelters with less or equal areal
+	router.get('/areal/max/:max', async (req, res) => {
+		const result = await sc.getSheltersByMaxAreal(req, res);
+		res.send(result);
+	});
 
-    // get shelters with less or equal areal
-    router.get('/areal/max/:max', async (req, res) => {
-        const result = await sc.getSheltersByMaxAreal(req, res);
-        res.send(result);
-    });
+	// get shelters with greater or equal capacity
+	router.get('/capacity/min/:min', async (req, res) => {
+		const result = await sc.getSheltersByMinCapacity(req, res);
+		res.send(result);
+	});
 
-    // get shelters with greater or equal capacity
-    router.get('/capacity/min/:min', async (req, res) => {
-        const result = await sc.getSheltersByMinCapacity(req, res);
-        res.send(result);
-    });
+	// get shelters with less or equal capacity
+	router.get('/capacity/max/:max', async (req, res) => {
+		const result = await sc.getSheltersByMaxCapacity(req, res);
+		res.send(result);
+	});*/
 
-    // get shelters with less or equal capacity
-    router.get('/capacity/max/:max', async (req, res) => {
-        const result = await sc.getSheltersByMaxCapacity(req, res);
-        res.send(result);
-    });
-
-    app.use(base, router);
-}
+	app.use(base, router);
+};
